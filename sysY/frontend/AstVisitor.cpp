2166b2b3e928f100c7163b493e7be066e1d23255
zhao <zhao.chenyuanmunich@outlook.com>
2022-08-15 09:02:03 +0800

    [Expr] global scalar var decl

--- sysY/frontend/AstVisitor.cpp
+++ sysY/frontend/AstVisitor.cpp
@@ -1,4 +1,8 @@
 #include "AstVisitor.hpp"
+#include <any>
+#include "llvm/Support/FormatVariadic.h"
+#include <string>
+#include <cstdlib>
 
 using namespace antlr4;
 using namespace llvm;
@@ -12,13 +16,67 @@ std::any AstVisitor::visitCompUnit(sysYParser::CompUnitContext *ctx)
 
 std::any AstVisitor::visitVarDecl(sysYParser::VarDeclContext *ctx)
 {
+    size_t tok = ctx->bType()->type->getType();
     for (auto vardef : ctx->varDef())
     {
-        
+        try
+        {
+            Value *v = std::any_cast<Value *>(visit(vardef));
+            v = ConstantInt::get(IntegerType::getInt16Ty(*TheContext), 1);
+        }
+        catch (std::bad_any_cast e)
+        {
+            errs() << "error: badcast\n";
+        }
     }
-    if (ctx->bType()->type->getType() == sysYParser::Int)
-    {
-        outs() << "a int decl\n";
-    }
+
     return nullptr;
+}
+
+std::any AstVisitor::visitInitVarDef(sysYParser::InitVarDefContext *ctx)
+{
+    std::string &&id = ctx->Identifier()->getText();
+    Value *v = std::any_cast<Value *>(visit(ctx->initVal()));
+    Constant *const_v = dyn_cast<Constant>(v);
+    if (nullptr == const_v)
+    {
+        errs() << "initializer must be a constant!\n";
+    }
+
+    GlobalVariable *global_var = dyn_cast<GlobalVariable>(TheModule->getOrInsertGlobal(id, IntegerType::getInt32Ty(*TheContext)));
+    //Constant* expr = BinaryConstantExpr();
+    if (Constant *derived = dyn_cast<Constant>(v))
+    {
+        global_var->setInitializer(derived);
+        global_var->setAlignment(Align{4});
+    }
+    return v;
+}
+
+std::any AstVisitor::visitUninitVarDef(sysYParser::UninitVarDefContext *ctx)
+{
+    return nullptr;
+}
+
+std::any AstVisitor::visitLVal(sysYParser::LValContext *ctx)
+{
+    return nullptr;
+}
+
+std::any AstVisitor::visitIntNumber(sysYParser::IntNumberContext *ctx)
+{
+    StringRef num_str{ctx->IntConst()->getText()};
+    uint8_t radix = 10;
+    if (num_str.startswith("0x") || num_str.startswith("0X"))
+        radix = 16;
+
+    Value *v = ConstantInt::get(IntegerType::getInt32Ty(*TheContext), num_str, radix);
+    return v;
+}
+
+std::any AstVisitor::visitFloatNumber(sysYParser::FloatNumberContext *ctx)
+{
+    StringRef num_str{ctx->FloatConst()->getText()};
+    Value *v = ConstantFP::get(Type::getFloatTy(*TheContext), num_str);
+    return v;
 }
--- sysY/frontend/AstVisitor.hpp
+++ sysY/frontend/AstVisitor.hpp
@@ -13,22 +13,43 @@
 #include "llvm/Support/Casting.h"
 
 namespace llvm{
+
+template<typename T>
+T cast_any(T value){
+  try
+        {
+            T v = std::any_cast<T>(value);
+            return v;
+        }
+        catch (std::bad_any_cast e)
+        {
+            errs()<<"error: badcast\n";
+        }
+}
+
 class AstVisitor : public sysYBaseVisitor
 {
 private:
-  LLVMContext TheContext;
+  std::unique_ptr<LLVMContext> TheContext;
   std::unique_ptr<Module> TheModule;
+  std::unique_ptr<IRBuilder<>> Builder;
   std::map<std::string, Value *> NamedValues;
 public:
   AstVisitor(const char* source_file_name){
-    TheModule = std::make_unique<Module>("test.ll", TheContext);
+    TheContext = std::make_unique<LLVMContext>();
+    TheModule = std::make_unique<Module>("test.ll", *TheContext);
     TheModule->setSourceFileName(source_file_name);
     TheModule->setTargetTriple("riscv64");
   }
   std::any visitCompUnit(sysYParser::CompUnitContext *ctx) override;
   std::any visitVarDecl(sysYParser::VarDeclContext *ctx) override;
-  //std::any visitUninitVarDef(sysYParser::UninitVarDefContext *ctx) override ;
-  //std::any visitInitVarDef(sysYParser::InitVarDefContext *ctx) override;
+  std::any visitUninitVarDef(sysYParser::UninitVarDefContext *ctx) override ;
+  std::any visitInitVarDef(sysYParser::InitVarDefContext *ctx) override;
+  //std::any visitScalarConstInitVal(sysYParser::ScalarConstInitValContext *ctx) override;
+  //std::any visitScalarInitVal(sysYParser::ScalarInitValContext *ctx) override;
+  std::any visitLVal(sysYParser::LValContext *ctx) override;
+  std::any visitFloatNumber(sysYParser::FloatNumberContext *ctx) override;
+  std::any visitIntNumber(sysYParser::IntNumberContext *ctx) override;
 };
 }
 
